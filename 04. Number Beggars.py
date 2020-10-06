1f07c5ecfba8d7c01bcb190cbf2a81bf4a2b9fd4
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-06 13:46:20 +0300

    Update 04. Number Beggars.py

--- 04. Number Beggars.py
+++ 04. Number Beggars.py
@@ -1,3 +1,18 @@
 n = input().split(', ')
 beggars = int(input())
 
+numbers = []
+
+for nums in n:
+    numbers.append(int(nums))
+  
+result = [0] * beggars
+
+
+for beggars_index in range(len(numbers)):
+    index = beggars_index % beggars
+    result[index] += numbers[beggars_index]
+
+print(result)  
+  
+                 
