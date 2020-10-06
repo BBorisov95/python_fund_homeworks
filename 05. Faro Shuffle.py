491cf1f2c34c4600e6eda536c3d012a9d5e31eec
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-06 16:14:18 +0300

    updated

--- 05. Faro Shuffle.py
+++ 05. Faro Shuffle.py
@@ -1,14 +1,14 @@
 cards = input().split()
-n_of_shuffles = int(input)
+n_of_shuffles = int(input())
 
 half = len(cards) // 2
 
-left_side = [:half]
-right_side = [half:]
+first_card = cards[0]
+last_card = cards[-1]
+
+left_side = cards[1:half]
+right_side = cards[half:-1]
 
 
-for shuffles in range(n_of_shuffles):
-    shuffled_deck = zip(right_side, left_side)
-    
-
-print(shuffled_deck)
+final_deck = []
+print(final_deck)
