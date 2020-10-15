bce450c3714365ec2304145c68eaca98ca2768ec
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-15 13:39:15 +0300

    Update 05. Electron-Distribution.py

--- 05. Electron-Distribution.py
+++ 05. Electron-Distribution.py
@@ -1,2 +1,14 @@
 electrons = int(input())
 
+electron_in_shells = []
+
+for electron in range(electrons)
+  for shell_index in electron_in_shells:
+    max_num_in_shell = 2 * shell_index**2
+      if electron_in_shells[shell_index] < max_num_in_shell
+         electron_in_shells[shell_index].append(electron)
+  
+#•	Maximum number of electrons in a shell is distributed with a rule of 2n^2 (n being position of a shell a.k.a. the list index + 1).
+#•	For example, maximum number of electrons in 3rd shield is 2*3^2 = 18.
+#•	Electrons should fill the lowest level shell first.
+#•	If the electrons have completely filled the lowest level shell, the other unoccupied electrons will fill the higher level shell and so on.
