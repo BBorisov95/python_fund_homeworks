2048e2b80f2aaa8aac16fbc564ade59bcc415489
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-15 13:04:34 +0300

    Update 04. Office-Chairs.py

--- 04. Office-Chairs.py
+++ 04. Office-Chairs.py
@@ -2,14 +2,16 @@ rooms = int(input())
 
 free_chairs = 0
 
-for room in range(rooms):
+for room in range(1, rooms + 1):
     chair, taken_chair = input().split(' ')
     taken_chair = int(taken_chair)
 
     if len(chair) >= taken_chair:
         free_chairs += len(chair) - taken_chair
-    elif len(chair) <= taken_chair:
+    else:
         needed_chairs = abs(taken_chair - len(chair))
         print(f'{needed_chairs} more chairs needed in room {room}')
-    else:
-        print(f'Game on, {free_chairs} free chairs left')
+        free_chairs = 0
+
+if free_chairs > 0:
+  print(f'Game On, {free_chairs} free chairs left')
