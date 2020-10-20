20f538263a528109ecbb72a1e8aa25d8f89754f1
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-20 17:44:11 +0300

    Create 10. Inventory.py

---
+++ 10. Inventory.py
@@ -0,0 +1,46 @@
+journal = input().split(', ')
+
+command = input()
+
+
+def collect(item):
+    if item not in journal:
+        journal.append(item)
+    return journal
+
+
+def drop(item):
+    if item in journal:
+       journal.remove(item)
+    return journal
+    
+    
+def renew(item):
+    if item in journal:
+       journal.remove(item)
+       journal.append(item)
+    return journal
+       
+  
+def combine_items(item):
+    old_item, new_item = item.split(':')
+    if old_item in journal:
+       journal.insert(journal.index('old_item')+1  ,new_item)
+    return journal
+
+while command != 'Craft!':
+    
+    action, item = command.split(' - ')
+    
+    if action = 'Collect':
+       collect(item)
+    elif action = 'Drop':
+       drop(item)
+    elif action = 'Combine Items':
+       combine_items(item)
+    elif action = 'Renew':
+       combine_items(item)
+
+    command = input()
+
+print(journal)
