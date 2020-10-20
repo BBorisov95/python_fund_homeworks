8d443f01a7811754fe516524bdbf13add6de06ad
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-20 13:13:10 +0300

    Update 09. Heart-Delivery.py

--- 09. Heart-Delivery.py
+++ 09. Heart-Delivery.py
@@ -3,35 +3,38 @@ int_neighborhood = list(map(int, neighborhood))
 
 command = input()
 
-cupid_position = None
+cupid_position = 0
 
 while command != 'Love!':
     
-    action, lenght = command.split()
+    jump, lenght = command.split()
     lenght = int(lenght)        
     
-    for home_index in range(0, len(int_neighborhood), lenght):
+    for home_index in range(0, len(int_neighborhood)):
         
-        if lenght > len(int_neighborhood):
+        cupid_position += lenght
+        
+        if cupid_position > len(int_neighborhood):
             home_index == 0
         
         if int_neighborhood[home_index] == 0:
             print(f'Place {home_index} already had Valentine\'s day.')
-        elif lenght <= int_neighborhood[home_index]:
+        elif cupid_position <= int_neighborhood[home_index]:
            int_neighborhood[home_index] -= 2
            if int_neighborhood[home_index] == 0:
                print(f'Place {home_index} has Valentine\'s day.')
                
-        cupid_position = home_index
+        cupid_position = home_index + 1
             
     
     command = input()
     
-print(f'Cupid\'s last position was {cupid_position}')
+print(f'Cupid\'s last position was {cupid_position}.')
+fail_homes = 0
+
 for homes in int_neighborhood:
-   fail_homes = 0
    if homes == 0:
       continue
    elif homes != 0:
        fail_homes += 1
-       print(f'Cupid has failed {fail_homes} places.')
+print(f'Cupid has failed {fail_homes} places.')
