f9dae8ebf1487014917c16b0c337669e3c732027
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-20 14:49:20 +0300

    update

--- 09. Heart-Delivery.py
+++ 09. Heart-Delivery.py
@@ -4,11 +4,15 @@ int_neighborhood = list(map(int, neighborhood))
 command = input()
 
 cupid_position = 0
-
+cr = 0
 while command != 'Love!':
     
     jump, lenght = command.split()
     lenght = int(lenght)
+
+    #for home_index in range(0, len(int_neighborhood)):
+
+    cupid_position = cr + lenght
         
     if cupid_position >= len(int_neighborhood):
        cupid_position = 0
@@ -20,6 +24,8 @@ while command != 'Love!':
         int_neighborhood[cupid_position] -= 2
         if int_neighborhood[cupid_position] == 0:
            print(f'Place {cupid_position} has Valentine\'s day.')
+
+    cr = cupid_position
 
             
     
